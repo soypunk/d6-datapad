e0e6159d371e17c2c8d613fea58793e845ebf070
Shawn Medero <shawn@medero.net>
2021-04-06 13:25:38 -0700

    updating prod settings.

--- d6_datapad/settings.py
+++ d6_datapad/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rainbow-unicrons-$jbcf_(=2bw=7)21@9(=e(o&3o$18nr)x=e9h82*nud6)k=vpf'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(76zi)cpf@1+5trrkaol1#^7+c-7qggk)v&zv&=%x*@irae+br')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['d6-datapad.medero.net','127.0.0.1']
 
