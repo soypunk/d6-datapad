8e18ba95f66ef54b653f75a0ccbda0a2684896ae
Shawn Medero <shawn@medero.net>
2021-04-08 14:53:36 -0700

    quality of life improvements for the admin.

--- d6_datapad/games/admin.py
+++ d6_datapad/games/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from .models import Game
 
 class GameAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'slug', 'created_by', 'created')
+    list_filter = ("created_by",)
+    search_fields = ['name']
+    prepopulated_fields = {'slug': ('name',)}
+    
+    def get_changeform_initial_data(self, request):
+        get_data = super(GameAdmin, self).get_changeform_initial_data(request)
+        get_data['created_by'] = request.user.pk
+        return get_data    
 
 admin.site.register(Game,GameAdmin) 
